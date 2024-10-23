3928676ea29c53567eba0f74d1bb13f822a5852a
Dial Syphax <dialsyphax@gmail.com>
2024-10-23 10:21:59 +0200

    Update pipelineCesi.groovy

--- pipelineCesi.groovy
+++ pipelineCesi.groovy
@@ -2,6 +2,19 @@ pipeline {
     agent any
 
     stages {
+        stage('Checkout') {
+            steps {
+                checkout([
+                    $class: 'GitSCM', 
+                    branches: [[name: '*/master']], 
+                    extensions: [], 
+                    userRemoteConfigs: [[
+                        credentialsId: 'JenkinsCesi', 
+                        url: 'https://github.com/Dialsyp/Test'
+                    ]]
+                ])
+            }
+        }
         stage('Hello') {
             steps {
                 echo 'Hello World'
